6e08943e390df460bed4c6263d6391c7ec17ee34
RattleRyny <126590217+RattleRyny@users.noreply.github.com>
2023-04-24 22:34:57 +0100

    asda

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,5 +1,8 @@
 var menu = document.getElementById('menu');
 var burger = document.getElementById('menu-button');
+var burgerMenuList = burgerMenuList.getElementById('burgerMenuList')
 
 burger.addEventListener("click", function() {
+    burgerMenuList.style.position = 'absolute';
+    burgerMenuList.style.right = '0px';
 } )
