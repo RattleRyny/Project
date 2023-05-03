3f3ea78fdaeb321cbe4efedca8540d10fed4bfc5
RattleRyny <126590217+RattleRyny@users.noreply.github.com>
2023-05-03 15:43:59 +0100

    yes

--- js/carousel.js
+++ js/carousel.js
@@ -1,143 +1,61 @@
-/*
-  ATTENTION:
-  DO NOT USE THIS CODE VERBATIM. You may base your solution
-  on it but make sure you use thorough commenting of your own
-  to demonstrate your understanding of the code. Un-commented
-  or copied code will receive a 0 grade.
-  
-  If you do not understand something in this code, refer to
-  previous labs, lecture slides and if you're still in doubt
-  email/Teams me your query.
-*/
+var images = document.getElementById('carouselImages');
+var caption = document.getElementById('carouselCaption');
+var prev = document.getElementById('carouselPrev');
+var next = document.getElementById('carouselNext');
 
-/*
-  This code is placed inside an event listener which
-  replaces the "defer" attribute on this script. In
-  your code, you will not need to wrap your code in
-  this function. Use the "defer" attribute instead!
-*/
 
-    /* -------------------------------
-      Get references to DOM Elements
-    ------------------------------- */
+fetch("/assets/carousel.json")
+
+
+.then(function(res) {
+
+
+  res.json().then(function(json) {
+
     
-    // Images container
-    var images = document.getElementById('carouselImages');
-    
-    // Image caption
-    var caption = document.getElementById('carouselCaption');
-    
-    // Previous image button
-    var prev = document.getElementById('carouselPrev');
-    
-    // Next image button
-    var next = document.getElementById('carouselNext');
-    
-    
-    /* ------------------------------
-      Fetch our gallery JSON object
-    ------------------------------ */
-    
-    // For a recap on using Fetch, please see my fetch demo:
-    // https://codepen.io/pbutcher/pen/wvaeQvZ
-    // The URL on the next line contains a JSON object you would normally include as a local asset e.g.: assets/gallery.json
-    // The images within this object would be local assets within an images folder e.g.: img/placeholder1.png
-    // See the Carousel demo video on Blackboard for a full example
-    fetch("/assets/gallery.json")
-    
-    // Do something with the response...
-    .then(function(res) {
-    
-      // Get the JSON representation of the response object then...
-      res.json().then(function(json) {
-    
-        // Loop over each object in our JSON object (array)...
-        json.forEach(function(el, i) {
-    
-          // Create a new image element...
-          var image = document.createElement('img');
-    
-          // Set some attributes...
-          image.setAttribute('src', el.url);        // The url of the image
-          image.setAttribute('alt', el.caption);    // The alternative text
-          image.setAttribute('title', el.caption);  // The tooltip
-    
-          // Append this image to our carouselImages element
-          images.appendChild(image);
-        });
-        
-        // Once our images are all loaded in, set up our carousel...
-        // We pass our JSON object (array) to this function
-        setupCarousel(json);
-      });
+    json.forEach(function(el, i) {
+
+      var image = document.createElement('img');
+      image.setAttribute('src', el.url);       
+      image.setAttribute('alt', el.caption);    
+      image.setAttribute('title', el.caption);  
+
+      
+      images.appendChild(image);
     });
     
-    
-    /* ----------------------------------
-      A function to set up our carousel
-    ---------------------------------- */
-    
-    // Carousel setup function (called from inside our fetch callback above)
-    // This function accepts the JSON object (array) of images as an argument
-    function setupCarousel(json) {
-      
-      /* -------------------------------------------
-        Set some variables to keep track of things
-      ------------------------------------------- */
-    
-      // Number of children in your carouselImages element
-      var imageCount = images.childElementCount;
-    
-      // Current image in view
-      var currentImage = 1;
-    
-      // Width of your images (could be calculated from clientWidth)
-      // var imageWidth = images.getElementsByTagName('img')[0].clientWidth;
-      var imageWidth = 1000;
-      
-      /* ----------------------------------------
-        Set some event listeners on our buttons
-      ---------------------------------------- */
-    
-      // Previous button
-      // Calls an anonymous function when the prev button is clicked
-      prev.addEventListener('click', function(e) {
-            e.preventDefault();
-        // If the image in view is not the first image...
-        if(currentImage != 1) {
-    
-          // Decrement the current image reference
-          --currentImage;
-    
-          // Move the previous image into view using the left property
-          images.style.left = imageWidth - (currentImage * imageWidth) + 'px';
-        }
-        
-        // Update our caption
-        caption.innerText = json[currentImage - 1].caption;
-      });
-    
-      // Next button
-      // Calls an anonymous function when the next button is clicked
-      next.addEventListener('click', function(e) {
+    setupCarousel(json);
+  });
+});
+
+
+function setupCarousel(json) {
+  var imageCount = images.childElementCount;
+  var currentImage = 1;
+  var imageWidth = 1000;
+  
+
+  prev.addEventListener('click', function(e) {
         e.preventDefault();
-    
-        // If the image in view is not the last image...
-        if(currentImage != imageCount) {
-    
-          // Increment the current image reference
-          ++currentImage;
-    
-          // Move the next image into view using the left property
-          images.style.left = imageWidth - (currentImage * imageWidth) + 'px';
-        }
-        
-        // Update our caption
-        caption.innerText = json[currentImage - 1].caption;
-      });
-      
-      // Update our caption
-      caption.innerText = json[currentImage - 1].caption;
+   
+    if(currentImage != 1) {
+      --currentImage;
+      images.style.left = imageWidth - (currentImage * imageWidth) + 'px';
     }
     
-    // The end of our DOMContentLoaded callback function
+    caption.innerText = json[currentImage - 1].caption;
+  });
+
+  next.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if(currentImage != imageCount) {
+
+      ++currentImage;
+      images.style.left = imageWidth - (currentImage * imageWidth) + 'px';
+    }
+    caption.innerText = json[currentImage - 1].caption;
+  });
+
+  caption.innerText = json[currentImage - 1].caption;
+}
---
+++ js/gallery.js
@@ -0,0 +1,34 @@
+var gallery = document.getElementById('gallery');
+
+fetch("https://assets.codepen.io/471256/gallery.json")
+
+.then(function(res) {
+
+  res.json().then(function(json) {
+
+    json.forEach(function(el) {
+      var galleryItem = document.createElement('a');
+    
+      galleryItem.setAttribute('class', 'gallery-item');
+      
+      galleryItem.setAttribute('href', el.url);
+      
+      galleryItem.setAttribute('target', '_blank');
+      
+      var image = document.createElement('img');
+
+      image.setAttribute('src', el.url);        
+      image.setAttribute('alt', el.caption);   
+      image.setAttribute('title', el.caption);  
+      
+      var caption = document.createElement('caption');
+      
+      caption.innerText = el.caption;
+
+      galleryItem.appendChild(image);
+      galleryItem.appendChild(caption);
+      
+      gallery.appendChild(galleryItem);
+    });
+  });
+});
