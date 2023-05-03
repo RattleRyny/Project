0c67ac89af3bb8d0686cf2d5c13c4f6ec720b73f
RattleRyny <126590217+RattleRyny@users.noreply.github.com>
2023-05-03 13:55:46 +0100

    Change

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -5,6 +5,7 @@ var burgerMenuList = document.getElementById('burgerMenuList');
 burger.addEventListener('click', function() {
     if(getComputedStyle(burgerMenuList, null).right == '-1000px') {
         burgerMenuList.style.right = '0px';
+        burgerMenuList.style.display = 'block';
     }
     else {
         burgerMenuList.style.right = '-1000px';
